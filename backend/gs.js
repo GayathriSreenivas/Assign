684a36c5bcf13b476cd0260f69bc5236a4d128e6
GayathriSreenivas <105072812+GayathriSreenivas@users.noreply.github.com>
2022-11-28 01:14:33 +0530

    Update gs.js

--- backend/gs.js
+++ backend/gs.js
@@ -1,7 +1,7 @@
 import twilio from 'twilio';
 
-const accountsid = 'AC8bca26b451c3fd1709a36e5c85e5e0ea';
-const authtoken = '290be7dbb8cfc99a46959c3ec5588a7c';
+const accountsid = 'your-twilio-auth-id';
+const authtoken = '<your-twilio-auth-token>';
 
 const client = new twilio(accountsid, authtoken);
 export default client;
